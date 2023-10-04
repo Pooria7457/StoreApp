5e6dfeb60f1c7339b4702014ae3231a061e828fe
Pooria 7457 <pooria.eb7457@gmail.com>
2023-10-04 20:39:09 +0330

    New Update

--- app/src/main/java/com/ebrahimipooria/storeapp/MainActivity.kt
+++ app/src/main/java/com/ebrahimipooria/storeapp/MainActivity.kt
@@ -5,35 +5,50 @@ import Api.RetrofitClient
 import Model.LoginModel
 import Model.ResponseLoginModel
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.ebrahimipooria.storeapp.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var sharedPreferences: SharedPreferences
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val edtUserName = findViewById<EditText>(R.id.edt_Main_UserName)
-        val edtPassword = findViewById<EditText>(R.id.edt_Main_Password)
-        val btnLogin = findViewById<Button>(R.id.btn_Main_Login)
+        sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
+        val sharedToken = sharedPreferences.getString("token", "")
+        if (sharedToken != "") {
+            val intent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+        }
 
-        val userNameText = edtUserName.text
-        val passwordText = edtPassword.text
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val userNameText = binding.edtMainUserName.text
+        val passwordText = binding.edtMainPassword.text
 
 
         val retrofit = RetrofitClient.getInstance()
         val apiInterface : ApiInterface = retrofit.create(ApiInterface::class.java)
 
-        btnLogin.setOnClickListener {
+        binding.btnMainLogin.setOnClickListener {
             val loginModel = LoginModel(userNameText.toString(),passwordText.toString())
 
             if(userNameText.toString().isEmpty()||passwordText.toString().isEmpty()){
@@ -47,10 +62,14 @@ class MainActivity : AppCompatActivity() {
                     ) {
                         if(response.isSuccessful){
                             val tokenId = response.body()!!.token
-                            Toast.makeText(this@MainActivity,tokenId,Toast.LENGTH_SHORT).show()
+                            sharedPreferences = getSharedPreferences("login", MODE_PRIVATE)
+                            val editor = sharedPreferences.edit()
+                            editor.putString("token", tokenId)
+                            editor.apply()
+                            val intent = Intent(this@MainActivity,HomeActivity::class.java)
+                            startActivity(intent)
                         }else{
-                            Toast.makeText(this@MainActivity,"Error : "+response.message(),Toast.LENGTH_SHORT).show()
-
+                            Toast.makeText(this@MainActivity,"Username Or Password Is Wrong",Toast.LENGTH_SHORT).show()
                         }
 
                     }
